fc415ce836ec9b9c74b67bd4db4125a600ecf10a
Sagar Ratan Garg <sagarratangarg@gmail.com>
2023-04-20 20:33:11 +0000

    first_try

--- posawesome_free/config/desktop.py
+++ posawesome_free/config/desktop.py
@@ -1,10 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from frappe import _
 
 def get_data():
 	return [
 		{
-			"module_name": "Posawesome Free",
+			"module_name": "POS Awesome",
+			"category": "Modules",
+			"color": "grey",
+			"icon": "octicon octicon-file-directory",
 			"type": "module",
-			"label": _("Posawesome Free")
+			"label": _("POS Awesome")
 		}
 	]
